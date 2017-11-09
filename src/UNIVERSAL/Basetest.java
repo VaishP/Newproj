8c05abff7183b0791814e1b4a29a47059348b8f9
TYSS <TYSS@TYSS-4F-019>
2017-11-09 10:00:41 +0530

    commit3

--- src/UNIVERSAL/Basetest.java
+++ src/UNIVERSAL/Basetest.java
@@ -19,7 +19,7 @@ public class Basetest implements Constants{
 		driver=new  FirefoxDriver();
 		driver.get("http://www.craftsvilla.com/");
 		driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 	
 	
